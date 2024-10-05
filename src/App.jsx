f9889d1fad51162f684b624bbcc9706059e8d5ef
ColdPlaces <35621749+ColdPlaces@users.noreply.github.com>
2024-10-05 07:51:14 +0000

    section 2 part 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,37 @@
+
+import React, {Component, useState} from "react";
+
+import ListCast from "./components/ListCast";
+
 function App() {
   const name = 'StarGazers'
+  let [memberInfo, setMemberInfo] = useState(null);
+
   return (
     <div className="container">
-      <article>
-        <hgroup>
-          <img src="images/group.svg" alt="StarGazers Group" />
-          <h1>Meet the <i style={{ color: "SteelBlue" }}>{name}</i></h1>
-          <p>Members of an <b>intergalactic alliance</b><br />
-            paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
-          <button className="outline" onClick={() => alert('Hi there')}>Click Me</button>
-        </hgroup>
-      </article>
+      <hgroup>
+        <img src="images/group.svg" alt="StarGazers Group" />
+        <h1>Meet the StarGazers</h1>
+        <p>Members of an <b>intergalactic alliance</b><br />
+          paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
+        <ListCast onChoice={(info) => {setMemberInfo(info)}}/>
+        { memberInfo &&
+          <artical>
+            <hgroup>
+              <div style={{
+                display: "flex",
+                gap: '1rem'
+              }}>
+              <img style={{width:'200px'}} src={`images/${memberInfo.slug}.svg`} alt={memberInfo.name} />
+              <hgroup>
+                <h1>{memberInfo.name}</h1>
+                <p>{memberInfo.bio}</p>                
+              </hgroup>
+              </div>
+            </hgroup>
+          </artical>
+        }
+      </hgroup>
     </div>
   )
 }
